67263a370c3e2ef9cb2f970371a528133cc6a644
LoicBrassart <loic.brassart@gmail.com>
2019-11-05 16:44:35 +0100

    Added backend support, everything works! \o/

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/users")
+      .then(({ data }) => {
+        console.log(data);
+        this.setState({
+          users: data
+        });
+      })
+      .catch(() => {});
+  }
+
   render() {
     return (
       <div>
         <h1>Mes users</h1>
         <ul>
-          <li>
-            <h3>Loic</h3>
-            <p>
-              <Link to="/user/1">Lien</Link>
-            </p>
-          </li>
+          {this.state.users.map(user => {
+            return (
+              <li>
+                <h3>{user.name}</h3>
+                <p>
+                  <Link to={`/user/${user.id}`}>Lien</Link>
+                </p>
+              </li>
+            );
+          })}
         </ul>
       </div>
     );
