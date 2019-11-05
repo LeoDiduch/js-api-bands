d53abb657db698c123f10f3638e17ddcf6b5238b
LoicBrassart <loic.brassart@gmail.com>
2019-11-05 15:47:06 +0100

    Link from one page to the other + use react-router(old school, lazy me iz lazy) to get my ID

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <p>Mes users</p>
+        <h1>Mes users</h1>
+        <ul>
+          <li>
+            <h3>Loic</h3>
+            <p>
+              <Link to="/user/1">Lien</Link>
+            </p>
+          </li>
+        </ul>
       </div>
     );
   }
--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 export default class UserProfile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    return <p>Les favoris de mon user</p>;
+    return <p>Les favoris de mon user #{this.props.match.params.id}</p>;
   }
 }
